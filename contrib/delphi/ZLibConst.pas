eca2075628c5884043fb2f425ca1b61ceeceb450
Nick Lauland <laulandn@gmail.com>
2025-04-28 09:10:48 -0500

    First commit

---
+++ contrib/delphi/ZLibConst.pas
@@ -0,0 +1,1 @@
+unit ZLibConst;interfaceresourcestring  sTargetBufferTooSmall = 'ZLib error: target buffer may be too small';  sInvalidStreamOp = 'Invalid stream operation';implementationend.
